d7e1850db49ac5e552bff53cfd6e5b00084a0f06
Durgesh Gupta <durgeshgupta@credenceanalytics.com>
2023-06-10 08:28:50 +0530

    deleted

--- src/routes/categoryRoute.js
+++
@@ -1,7 +0,0 @@
-const router=require('express').Router();
-const category=require('../controllers/categoryController');
-
-router.post('/create',category.createCategory);
-router.get('/list',category.listCategory);
-
-module.exports=router;
--- src/server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const app=require('./app')
-const PORT=2100;
-
-app.listen(PORT,()=>{
-    console.log(`Server listening on port ${PORT}`)
-});
